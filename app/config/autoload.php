29a3cf02a1e14b9614fb2a8463348e4a5fea42f8
RON <h4ckw1z@gmail.com>
2024-07-30 12:20:00 +0800

    update config.php file

--- app/config/autoload.php
+++ app/config/autoload.php
@@ -81,7 +81,7 @@ $autoload['libraries'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helpers'] = array('url');
+$autoload['helpers'] = array();
 
 /*
 | -------------------------------------------------------------------
