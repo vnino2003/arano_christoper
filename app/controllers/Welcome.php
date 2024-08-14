6063cdfd11cfc6c6f0bc9ba56bc0c336b5b478c2
RON <h4ckw1z@gmail.com>
2024-08-14 08:29:35 +0800

    Update Config Class

--- app/controllers/Welcome.php
+++ app/controllers/Welcome.php
@@ -3,9 +3,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 class Welcome extends Controller {
 	public function index() {
-		//$this->call->view('welcome_page');
-		$this->config->load('config');
-		echo $this->config->get('base_url');
+		$this->call->view('welcome_page');
 	}
 }
 ?>
