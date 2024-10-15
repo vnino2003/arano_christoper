b7a7894c102f5b1a43a304aaf03bee4e04fc16b5
RON <h4ckw1z@gmail.com>
2024-10-15 17:11:28 +0800

    update invoker bug

--- app/views/welcome_page.php
+++ app/views/welcome_page.php
@@ -82,7 +82,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
         <p>You can star and fork the <a href="https://github.com/ronmarasigan/LavaLust">Github Repository</a> and read its <a href="https://lavalust4.netlify.app/">Documentation</a>.</p>
     </div>
-    <div class="footer">Page rendered with <?php echo $this->performance->memory_usage(); ?> in <strong><?php echo $this->performance->elapsed_time('lavalust'); ?></strong> seconds. <?php echo  (config_item('ENVIRONMENT') === 'development') ?  'LavaLust Version <strong>' . config_item('VERSION') . '</strong>' : '' ?>
+    <div class="footer">Page rendered with <?php echo lava_instance()->performance->memory_usage(); ?> in <strong><?php echo lava_instance()->performance->elapsed_time('lavalust'); ?></strong> seconds. <?php echo  (config_item('ENVIRONMENT') === 'development') ?  'LavaLust Version <strong>' . config_item('VERSION') . '</strong>' : '' ?>
     </div>
 
 </body>
